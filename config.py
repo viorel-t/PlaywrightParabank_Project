1ea3c2385ccded56a36a1a5fd8dc361e09eb0a5d
DESKTOP-A0VAF9P\admins1 <viorel.tanasan@gmail.com>
2026-04-27 23:42:31 +0300

    Corrected env variables

--- config.py
+++ config.py
@@ -6,5 +6,5 @@
 
 import os
 
-URL_BAZA = os.getenv("URL_BAZA", "http://localhost:8080/parabank/")
-URL_BAZA_API = os.getenv("URL_BAZA_API", "http://localhost:8080/parabank/services/bank/")
+URL_BAZA = os.getenv("BASE_URL", "http://localhost:8080/parabank/")
+URL_BAZA_API = os.getenv("BASE_URL_API", "http://localhost:8080/parabank/services/bank/")
