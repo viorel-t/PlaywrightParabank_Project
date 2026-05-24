5fd3b1c32095c8d29b39a4ee6ab8b91eeaa77505
v-tanasan <vasile.tanasan@student.usv.ro>
2026-05-24 23:23:09 +0300

    modified search field "toDate" in transactions search (conftest.py)

--- tests/conftest.py
+++ tests/conftest.py
@@ -198,7 +198,7 @@ def payment_data_load():
 def transaction_data_load():
     return {
         "fromDate": "04-01-2026", # MM-DD-YYYY
-        "toDate": "05-01-2026",   # MM-DD-YYYY
+        "toDate": "05-24-2026",   # MM-DD-YYYY
         "amount": "10",
         "from_account": "13344",
     }
